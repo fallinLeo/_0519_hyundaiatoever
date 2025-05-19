7aacda0f27ac846a00c6652cfea80e5694765a96
dltmdwn0325 <dltmdwn0325@gmail.com>
2025-05-19 11:49:28 +0900

    update_main_cell

--- tetris_r4/main.c
+++ tetris_r4/main.c
@@ -17,18 +17,6 @@
 #define RESIZE_COLS (DEFAULT_COLS-30) //=90
 #define RESIZE_ROWS 30
 
-typedef struct _cell_t {
-    point_t point; // cell의 위치
-    color_t color; // cell의 색상
-    cell_attributes_t att; // cell의 속성
-} cell_t;
-
-void cell_init(cell_t* cell, int x, int y, cell_attributes_t att, color_t color) {
-    cell->point.x = x;
-    cell->point.y = y;
-    cell->att = att;
-    cell->color = color;
-}
 
 //void clearPreviousPosition(int x, int y) {
 //    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -36,22 +24,6 @@ void cell_init(cell_t* cell, int x, int y, cell_attributes_t att, color_t color)
 //    DWORD written;
 //    FillConsoleOutputCharacter(hConsole, ' ', 1, pos, &written); // 이전 위치를 공백으로 덮음
 //}
-
-//void draw_one_cell(cell_t* cell, uint32_t x, uint32_t y) {
-void cell_draw(cell_t* cell, uint32_t x, uint32_t y) {
-    cell->point.x = x;
-    cell->point.y = y;
-
-    COORD pos;
-    pos.X = (short)cell->point.x;
-    pos.Y = (short)cell->point.y;
-    pos.X *= 2;
-
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleCursorPosition(hConsole, pos);
-    printf("■");
-}
-
 
 void block_draw(block_t* block) {
     //blk->x = x;
@@ -103,19 +75,6 @@ void block_draw(const block_t* blk, uint32_t x, uint32_t y) {
     }
 }
 */
-
-
-void cell_draw_test(windows_console_t* console) {
-    cell_t my_cell;
-    cell_init(&my_cell, 0, 0, N, YELLOW); // 좌표계 점검용 테스트 코드
-    console_set_fore_color(console, my_cell.color);
-    //console->fore_color(my_cell.color);
-    cell_draw(&my_cell, 0, 0); // 좌상단
-    cell_draw(&my_cell, RESIZE_COLS / 2 - 1, 0); // 우상단
-    cell_draw(&my_cell, RESIZE_COLS / 2 - 1, RESIZE_ROWS - 1); // 우하단
-    cell_draw(&my_cell, 0, RESIZE_ROWS - 1); // 좌하단
-    cell_draw(&my_cell, (RESIZE_COLS / 2 - 1) / 2, (RESIZE_ROWS - 1) / 2); // 정중앙
-}
 
 bool running = true;
 
