8dab1daf2f0478f608be3a813463adbe403bda92
Minhee-1234 <valsgml@naver.com>
2025-05-19 14:31:55 +0900

    finish

--- tetris_r4/board.h
+++ tetris_r4/board.h
@@ -15,33 +15,6 @@
 #define DIR_DOWN 2
 #define DIR_LEFT 3
 #define DIR_RIGHT 4
-// 전체 보드 전역 변수
-extern cell_t gboard[BOARD_HEIGHT][BOARD_WIDTH] = { 0, };
-
-// 블럭의 초기 정보를 갖고 있는 데이터
-cell_attributes_t board_data_attributes[BOARD_HEIGHT][BOARD_WIDTH] = {
-    { W, E, E, E, E, E, E, E, E, E, E, W }, // (0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7),(0,8),(0,9),(0,10),(0,11)
-    { W, E, E, E, E, E, E, E, E, E, E, W }, // (1,0),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(1,9),(1,10),(1,11)
-    { W, E, E, E, E, E, E, E, E, E, E, W }, // (2,0),(2,1),(2,2),(2,3),(2,4),(2,5),(2,6),(2,7),(2,8),(2,9),(2,10),(2,11)
-    { W, E, E, E, E, E, E, E, E, E, E, W }, // (3,0),(3,1),(3,2),(3,3),(3,4),(3,5),(3,6),(3,7),(3,8),(3,9),(3,10),(3,11)
-    { W, E, E, E, E, E, E, E, E, E, E, W }, // (4,0),(4,1),(4,2),(4,3),(4,4),(4,5),(4,6),(4,7),(4,8),(4,9),(4,10),(4,11)
-    { W, E, E, E, E, E, E, E, E, E, E, W }, // (5,0),(5,1),(5,2),(5,3),(5,4),(5,5),(5,6),(5,7),(5,8),(5,9),(5,10),(5,11)
-    { W, E, E, E, E, E, E, E, E, E, E, W }, // (6,0),(6,1),(6,2),(6,3),(6,4),(6,5),(6,6),(6,7),(6,8),(6,9),(6,10),(6,11)
-    { W, E, E, E, E, E, E, E, E, E, E, W }, // (7,0),(7,1),(7,2),(7,3),(7,4),(7,5),(7,6),(7,7),(7,8),(7,9),(7,10),(7,11)
-    { W, E, E, E, E, E, E, E, E, E, E, W },
-    { W, E, E, E, E, E, E, E, E, E, E, W },
-    { W, E, E, E, E, E, E, E, E, E, E, W },
-    { W, E, E, E, E, E, E, E, E, E, E, W },
-    { W, E, E, E, E, E, E, E, E, E, E, W },
-    { W, E, E, E, E, E, E, E, E, E, E, W },
-    { W, E, E, E, E, E, E, E, E, E, E, W },
-    { W, E, E, E, E, E, E, E, E, E, E, W },
-    { W, E, E, E, E, E, E, E, E, E, E, W },
-    { W, E, E, E, E, E, E, E, E, E, E, W },
-    { W, E, E, E, E, E, E, E, E, E, E, W }, // (18,0),(18,1),(18,2),(18,3),(18,4),(18,5),(18,6),(18,7),(18,8),(18,9),(18,10),(18,11)
-    { W, E, E, E, E, E, E, E, E, E, E, W }, // (19,0),(19,1),(19,2),(19,3),(19,4),(19,5),(19,6),(19,7),(19,8),(19,9),(19,10),(19,11)
-    { W, W, W, W, W, W, W, W, W, W, W, W }, // (20,0),(20,1),(20,2),(20,3),(20,4),(20,5),(20,6),(20,7),(20,8),(20,9),(20,10),(20,11)
-};
 
 #define BOARD_START_X (4)
 #define BOARD_START_Y (4)
@@ -50,6 +23,11 @@ cell_attributes_t board_data_attributes[BOARD_HEIGHT][BOARD_WIDTH] = {
 // ■          ■ // (0,2),
 // .............. 
 // ■■■■■■■ // (0,n),
+
+// 전체 보드 전역 변수
+extern cell_t gboard[BOARD_HEIGHT][BOARD_WIDTH];
+// 블럭의 초기 정보를 갖고 있는 데이터
+extern cell_attributes_t board_data_attributes[BOARD_HEIGHT][BOARD_WIDTH];
 
 void board_init(int data[BOARD_HEIGHT][BOARD_WIDTH]);
 void board_display_info(cell_t board[BOARD_HEIGHT][BOARD_WIDTH]);
