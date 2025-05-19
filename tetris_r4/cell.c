5638487251e36956dfcc625e0dce0ae9e84afbde
dltmdwn0325 <dltmdwn0325@gmail.com>
2025-05-19 11:41:01 +0900

    cell_update

--- tetris_r4/cell.c
+++ tetris_r4/cell.c
@@ -1,1 +1,36 @@
-// 셀 단위의 색상, 위치, 그리기 관련
+// cell.c
+#include "cell.h"
+#include <stdio.h>
+#include "console.h"
+
+void cell_init(cell_t* cell, int x, int y, cell_attributes_t att, color_t color) {
+    cell->point.x = x;
+    cell->point.y = y;
+    cell->att = att;
+    cell->color = color;
+}
+
+void cell_draw(cell_t* cell, uint32_t x, uint32_t y) {
+    cell->point.x = x;
+    cell->point.y = y;
+
+    COORD pos;
+    pos.X = (short)(cell->point.x * 2);
+    pos.Y = (short)(cell->point.y);
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hConsole, pos);
+    printf("■");
+}
+
+void cell_draw_test(windows_console_t* console) {
+    cell_t my_cell;
+    cell_init(&my_cell, 0, 0, N, YELLOW);
+    console_set_fore_color(console, my_cell.color);
+
+    cell_draw(&my_cell, 0, 0); // 좌상단
+    cell_draw(&my_cell, RESIZE_COLS / 2 - 1, 0); // 우상단
+    cell_draw(&my_cell, RESIZE_COLS / 2 - 1, RESIZE_ROWS - 1); // 우하단
+    cell_draw(&my_cell, 0, RESIZE_ROWS - 1); // 좌하단
+    cell_draw(&my_cell, (RESIZE_COLS / 2 - 1) / 2, (RESIZE_ROWS - 1) / 2); // 중앙
+}
